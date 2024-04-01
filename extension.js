887faaff9b74e6cf00841db26cb02e4a5f9e6e41
rmmurray11 <59099199+rmmurray11@users.noreply.github.com>
2024-04-01 13:31:22 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -171,7 +171,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'Items will appear one at a time.<br/><br/>' + 
 				'If you see <font color="#ff0000"><b>TRY AGAIN</b></font>. ' +
 				'Press the other key to continue.<br/><br/>' + 
-				'<p align="center">Press the <b>space bar</b> when you are ready to start. Please respond as <b>QUICKLY<b> and as <b>ACCURATELY<b> as possible</font></p></div>', 
+				'<p align="center">Press the <b>space bar</b> when you are ready to start. Please respond as <b>QUICKLY<b> and as <b>ACCURATELY<b> as possible </font></p></div>', 
 			instTemplateCategoryRight : '<div><p align="center" style="font-size:20px; font-family:arial">' +
 				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
@@ -183,7 +183,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'Items will appear one at a time.<br/><br/>' + 
 				'If you see <font color="#ff0000"><b>TRY AGAIN</b></font>. ' + 
 				'Press the other key to continue.<br/><br/>' + 
-				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>', 
+				'<p align="center">Press the <b>space bar</b> when you are ready to start. Please respond as <b>QUICKLY<b> and as <b>ACCURATELY<b> as possible </font></p></div>', 
 			instTemplateCategoryLeft : '<div><p align="center" style="font-size:20px; font-family:arial">' +
 				'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' + 
 				'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
@@ -195,7 +195,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				'Items will appear one at a time.<br/><br/>' + 
 				'If you see <font color="#ff0000"><b>TRY AGAIN</b></font>. ' +
 				'Press the other key to continue.<br/><br/>' + 
-				'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>', 
+				'<p align="center">Press the <b>space bar</b> when you are ready to start. Please respond as <b>QUICKLY<b> and as <b>ACCURATELY<b> as possible </font></p></div>', 
 			
 			//The default feedback messages for each cutoff. 
 			//If you put attribute1, attribute2 and category here, 
